97e511aa430c975e7b4ea34ec75ebb616c6a3a1f
Mehedyh259 <mehedyh259@gmail.com>
2022-06-01 21:40:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1pxy4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async () => {
+    const bookingCollection = client.db("hotel-reserve").collection("booking");
+
+    try {
+        await client.connect();
+        console.log('db connected');
+
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req?.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.status(201).send(result);
+        });
+
+        app.get('/booking', async (req, res) => {
+            const bookings = await bookingCollection.find().toArray();
+            res.status(200).send(bookings);
+        })
+
+
+
+
+
+    } finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("server is running");
+});
+
+app.listen(port, () => console.log('app running'))
+
