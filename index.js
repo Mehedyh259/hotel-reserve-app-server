bdb4db8cd64f961a099e56b228f5417394da3e1a
Mehedyh259 <mehedyh259@gmail.com>
2022-06-01 22:05:25 +0600

    error testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express();
@@ -12,35 +12,35 @@ const port = 5000 || process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1pxy4.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1pxy4.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-const run = async () => {
+// const run = async () => {
 
-    try {
-        await client.connect();
-        console.log('db connected');
-        const bookingCollection = client.db("hotel-reserve").collection("booking");
+//     try {
+//         await client.connect();
+//         console.log('db connected');
+//         const bookingCollection = client.db("hotel-reserve").collection("booking");
 
-        app.post('/booking', async (req, res) => {
-            const booking = req?.body;
-            const result = await bookingCollection.insertOne(booking);
-            res.status(201).send(result);
-        });
+//         app.post('/booking', async (req, res) => {
+//             const booking = req?.body;
+//             const result = await bookingCollection.insertOne(booking);
+//             res.status(201).send(result);
+//         });
 
-        app.get('/booking', async (req, res) => {
-            const bookings = await bookingCollection.find().toArray();
-            res.status(200).send(bookings);
-        })
+//         app.get('/booking', async (req, res) => {
+//             const bookings = await bookingCollection.find().toArray();
+//             res.status(200).send(bookings);
+//         })
 
 
-    } finally {
+//     } finally {
 
-    }
+//     }
 
-}
-run().catch(console.dir);
+// }
+// run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Hotel Reserve app server");
